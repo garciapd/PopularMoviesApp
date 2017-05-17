2950be254ab9c32f4169bdc2a772b078b9f62d12
Daniel García <dani.garcia.perez@gmail.com>
2017-05-17 20:57:16 +0200

    Added Favorite button in the detail view that modifies the list in the database

--- app/src/main/java/com/danielgarciaperez/nanodegree/popularmoviesapp/MovieDetail.java
+++ app/src/main/java/com/danielgarciaperez/nanodegree/popularmoviesapp/MovieDetail.java
@@ -1,13 +1,21 @@
 package com.danielgarciaperez.nanodegree.popularmoviesapp;
 
+import android.content.ContentValues;
 import android.content.Intent;
+import android.database.Cursor;
 import android.databinding.DataBindingUtil;
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.v4.app.LoaderManager;
+import android.support.v4.content.CursorLoader;
+import android.support.v4.content.Loader;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.danielgarciaperez.nanodegree.popularmoviesapp.data.MovieContract;
 import com.danielgarciaperez.nanodegree.popularmoviesapp.databinding.MovieDetailBinding;
 import com.danielgarciaperez.nanodegree.popularmoviesapp.model.Movie;
 import com.google.gson.Gson;
@@ -17,11 +25,20 @@ import com.squareup.picasso.Picasso;
  * Created by danielgarciaperez on 13/05/2017.
  */
 
-public class MovieDetail extends AppCompatActivity {
+public class MovieDetail extends AppCompatActivity implements LoaderManager.LoaderCallbacks<Cursor>{
 
     public static final String MOVIE = "movie";
 
+    private static final int TASK_LOADER_ID = 0;
+    private static final String[] PROJECTION = new String[] { "_id" };
+
     private MovieDetailBinding movieDetailBinding;
+
+    private Movie movie;
+
+    private String movieJson;
+
+    private boolean isFavorite = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,8 +49,12 @@ public class MovieDetail extends AppCompatActivity {
 
         Intent intent = getIntent();
         Bundle extras = intent.getExtras();
+        movieJson = extras.getString(MOVIE);
         Gson gson = new Gson();
-        Movie movie = gson.fromJson(extras.getString(MOVIE), Movie.class);
+        movie = gson.fromJson(movieJson, Movie.class);
+
+        isFavorite = false;
+        getSupportLoaderManager().initLoader(TASK_LOADER_ID, null, MovieDetail.this);
 
         fillView(movie);
     }
@@ -44,5 +65,64 @@ public class MovieDetail extends AppCompatActivity {
         movieDetailBinding.date.setText(movie.getReleaseDate().substring(0,4));
         movieDetailBinding.vote.setText(movie.getVoteAverage()+"/10");
         movieDetailBinding.synopsis.setText(movie.getOverview());
+
+        movieDetailBinding.button.setVisibility(View.INVISIBLE);
+    }
+
+    private void fillMarkFavoriteButton(boolean isFavorite){
+        if(isFavorite){
+            movieDetailBinding.button.setText(getString(R.string.markedFavorite));
+        }else{
+            movieDetailBinding.button.setText(getString(R.string.markFavorite));
+        }
+
+        movieDetailBinding.button.setVisibility(View.VISIBLE);
+    }
+
+    public void toogleFavorite(View v){
+        String id = Integer.toString(movie.getId());
+        Uri uri = MovieContract.MovieEntry.CONTENT_URI;
+
+
+        if(isFavorite){
+            uri = uri.buildUpon().appendPath(id).build();
+            int count = getContentResolver().delete(uri, null, null);
+        }else{
+            ContentValues values = new ContentValues();
+            values.put(MovieContract.MovieEntry._ID, id);
+            values.put(MovieContract.MovieEntry.COLUMN_MOVIE, movieJson);
+            Uri newUri = getContentResolver().insert(uri,values);
+        }
+
+        isFavorite = !isFavorite;
+        fillMarkFavoriteButton(isFavorite);
+    }
+
+    @Override
+    public Loader<Cursor> onCreateLoader(int id, Bundle args) {
+        String selection = "_id=?";
+        String[] selectionArgs = new String[]{movie.getId().toString()};
+        return new CursorLoader(MovieDetail.this, MovieContract.MovieEntry.CONTENT_URI,
+                PROJECTION, selection, selectionArgs, null);
+    }
+
+    @Override
+    public void onLoadFinished(Loader<Cursor> loader, Cursor data) {
+        isFavorite = false;
+        if(data != null && data.getCount() > 0){
+            isFavorite = true;
+        }
+        fillMarkFavoriteButton(isFavorite);
+    }
+
+    @Override
+    public void onLoaderReset(Loader<Cursor> loader) {
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        getSupportLoaderManager().restartLoader(TASK_LOADER_ID, null, this);
     }
 }
